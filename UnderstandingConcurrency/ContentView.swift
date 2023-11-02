ccaf002278c02cca908710a82b7109c822b5f002
Freddy <fredericdouamba2014@gmail.com>
2023-11-02 06:03:34 -0700

    Initial Commit

---
+++ UnderstandingConcurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnderstandingConcurrency
+//
+//  Created by Student Account on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnderstandingConcurrency/UnderstandingConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingConcurrencyApp.swift
+//  UnderstandingConcurrency
+//
+//  Created by Student Account on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
